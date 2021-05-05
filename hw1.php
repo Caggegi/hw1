eb10f231a9c586b664fec61fbca312ac12a328fc
Caggegi <rosariocaggegi99@gmail.com>
2021-05-05 16:00:39 +0200

    upload base

---
+++ hw1.php
@@ -0,0 +1,153 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8"/>
+        <link href="style/hw1.css" rel="stylesheet"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>VideoTube o46002042</title>
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@700&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=PT+Sans&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono&display=swap" rel="stylesheet">
+        <script src="javascript/contents.js " defer></script>
+        <script src="javascript/script.js" defer></script>
+    </head>
+    <body>
+        <div class="menu_priority hide"></div>
+        <div class="icon_menu hide">
+            <div class="m_header">
+                <div class="window_buttons">
+                    <div class="close_button"></div>
+                    <div class="save_button"></div>
+                </div>
+                <div class="form_container">
+                    <form id="choose_category">
+                        <input type="text" placeholder="Categoria" id="category">
+                        <input type="submit" id="send" value="Cerca">
+                    </form>
+                </div>
+            </div>
+            <div class="m_body">
+                <div class="current">
+                    <img id="current_picture">
+                    <div>
+                        <input type="text" placeholder="Name" id="current_name"></input>
+                        <input type="text" placeholder="description" id="current_description"></input>
+                    </div>
+                </div>
+                <h2>Seleziona</h2>
+                <div class="pick">
+                    
+                </div>
+            </div>
+        </div>
+        <header>
+            <div id="background"></div>
+            <div id="overlay"></div>
+            <div id="search"> 
+                <input type="text" placeholder="Cerca" id="search">
+                </input>
+                <div>
+                    <img src="https://raw.githubusercontent.com/Caggegi/mhw1/master/img/magnify.svg"/>
+                    <img class="mobile" src="img/icons/account-circle-outline.svg"/>
+                </div>
+            </div>
+            <div id="info">
+                <div id="account">
+                    <div>
+                        <h3>Default Name</h3>
+                        <p>Default Description</p>
+                    </div>
+                    <img src="img/icons/account-circle-outline.svg"/>
+                </div>
+            </div>
+        </header>
+        <div id="main">
+            <nav>
+                    <div class="nav_button" id="home">
+                        <img src="https://raw.githubusercontent.com/Caggegi/mhw1/master/img/Dark/home-outline.svg" class="desktop">
+                        <img src="https://raw.githubusercontent.com/Caggegi/mhw1/master/img/Light/home-outline.svg" class="mobile">
+                        <h4>Home</h4>
+                    </div>
+                    <div class="nav_button">
+                        <img src="https://raw.githubusercontent.com/Caggegi/mhw1/master/img/Dark/clock-time-four-outline.svg" class="desktop">
+                        <img src="https://raw.githubusercontent.com/Caggegi/mhw1/master/img/Light/clock-time-four-outline.svg" class="mobile">
+                        <h4>Recenti</h4>
+                    </div>
+                    <div class="nav_button">
+                        <img src="https://raw.githubusercontent.com/Caggegi/mhw1/master/img/Dark/trending-up.svg" class="desktop">
+                        <img src="https://raw.githubusercontent.com/Caggegi/mhw1/master/img/Light/trending-up.svg" class="mobile">
+                        <h4>Tendenze</h4>
+                    </div>
+                    <div class="nav_button" id="preferiti">
+                        <img src="https://raw.githubusercontent.com/Caggegi/mhw1/master/img/Dark/heart-outline.svg" class="desktop">
+                        <img src="https://raw.githubusercontent.com/Caggegi/mhw1/master/img/Light/heart-outline.svg" class="mobile">
+                        <h4>Preferiti</h4>
+                    </div>
+                <div>
+                    <div class="nav_button">
+                        <img src="https://raw.githubusercontent.com/Caggegi/mhw1/master/img/Dark/account-multiple-check-outline.svg" class="desktop">
+                        <img src="https://raw.githubusercontent.com/Caggegi/mhw1/master/img/Light/account-multiple-check-outline.svg" class="mobile">
+                        <h4>Iscrizioni</h4>
+                    </div>
+                    <div class="subscription">
+                        <img src="https://raw.githubusercontent.com/Caggegi/mhw1/master/img/others/profile.png">
+                        <h5>Creator 1</h5>
+                    </div>
+                    <div class="subscription">
+                        <img src="https://raw.githubusercontent.com/Caggegi/mhw1/master/img/others/profile2.jpg">
+                        <h5>Creator 2</h5>
+                    </div>
+                    <div class="subscription">
+                        <img src="https://raw.githubusercontent.com/Caggegi/mhw1/master/img/others/profile3.jpg">
+                        <h5>Creator 3</h5>
+                    </div>
+                    <div class="subscription">
+                        <img src="https://raw.githubusercontent.com/Caggegi/mhw1/master/img/others/profile4.jpg">
+                        <h5>Creator 4</h5>
+                    </div>
+                </div>
+            </nav>
+            <article id="video-frame">
+                <section class="genre hide" id="ricerca">
+                    <h2>Ricerca</h2>
+                    <div class="show-case"></div>
+                </section>
+                <section class="genre hide" id="preferiti">
+                    <h2>Preferiti</h2>
+                    <h3 id="no_pref">Nessun preferito trovato</h3>
+                    <div class="show-case"></div>
+                </section>
+                <section class="genre" id="film">
+                    <h2>Film</h2>
+                    <div class="show-case"></div>
+                </section>
+                <section class="genre" id="musica">
+                    <h2>Musica</h2>
+                    <div class="show-case"></div>
+                </section>
+                <section class="genre" id="gameplay">
+                    <h2>Gameplay</h2>
+                    <div class="show-case"></div>
+                </section>
+                <section class="genre" id="altro">
+                    <h2>Altro</h2>
+                    <div class="show-case"></div>
+                </section> 
+            </article>
+        </div>
+        <footer>
+            <div id="social">
+                <a href="https://www.facebook.com/rosario.caggegi.142" target="about:blank"><img src="https://raw.githubusercontent.com/Caggegi/mhw1/master/img/Light/facebook.svg"></a>
+                <a href="https://www.instagram.com/rosario.caggegi/" target="about:blank"><img src="https://raw.githubusercontent.com/Caggegi/mhw1/master/img/Light/instagram.svg"></a>
+                <a href="https://github.com/Caggegi/mhw3" target="about:blank"><img src="https://raw.githubusercontent.com/Caggegi/mhw1/master/img/Light/github.svg"></a>
+            </div>
+            <div>
+                <p>Questo sito web è stato creato da Rosario Caggegi o46002042 usando HTML e CSS</p>
+            </div>
+        </footer>
+    </body>
+</html>
---
+++ javascript/contents.js
@@ -0,0 +1,73 @@
+const video = [
+    {
+       titolo: 'Il Re Leone',
+       immagine: 'https://lumiere-a.akamaihd.net/v1/images/b_thelionking2019_header_poststreet_18276_ada305ce.jpeg?region=0,0,2048,879',
+       creator: 'Disney',
+       descrizione: 'The Lion King is a 1994 American animated musical drama film produced by Walt Disney Feature Animation and released by Walt Disney Pictures. It is the 32nd Disney animated feature film, and the fifth animated film produced during a period known as the Disney Renaissance...',
+       id: '000',
+       tipo: 'film'
+    },
+    {
+         titolo: 'Soul',
+         immagine: 'https://www.nerdpool.it/wp-content/uploads/2021/03/soul-cover-disney-pixar-np.jpeg',
+         creator: 'Disney',
+         descrizione: 'Soul is a 2020 American computer-animated fantasy comedy-drama film produced by Pixar Animation Studios and released by Walt Disney Pictures...',
+         id: '001',
+         tipo: 'film'
+    },
+    {
+         titolo: 'Collateral Beauty',
+         immagine: 'https://www.latestatamagazine.it/wp-content/uploads/2021/01/Collateral-Beauty.jpg',
+         creator: 'Warner Bros. Pictures',
+         descrizione: 'Collateral Beauty is a 2016 American fantasy drama film directed by David Frankel and written by Allan Loeb. The film stars an ensemble cast of Will Smith, Edward Norton, Keira Knightley, Michael Peña, Naomie Harris, Jacob Latimore, Kate Winslet, and Helen Mirren.',
+         id: '002',
+         tipo: 'film'
+    },
+    {
+        titolo: 'Voce',
+        immagine: 'https://www.oaplus.it/wp-content/uploads/2021/03/Madame-Cover-Voce-Singolo-Sanremo-2021.jpg',
+        creator: 'Madame',
+        descrizione: 'Mi ricordo di te \nRicordo i mille giri sulle giostre, su di te\nHo fatto un\'altra canzone, mi ricorda chi sono\nHo messo un altro rossetto sopra il labbro superiore...',
+        id: '001',
+        tipo: 'musica'
+    },
+    {
+          titolo: 'Never Gonna Give You Up',
+          immagine: 'https://i.ytimg.com/vi/dQw4w9WgXcQ/maxresdefault.jpg',
+          creator: 'Rick Astley',
+          descrizione: '"Never Gonna Give You Up" is the debut single recorded by English singer and songwriter Rick Astley, released on 27 July 1987...',
+          id: '002',
+          tipo: 'musica'
+    },
+    {
+          titolo: 'Twisted Transistor',
+          immagine: 'https://upload.wikimedia.org/wikipedia/en/thumb/e/e8/Twisted_Transistor.jpg/220px-Twisted_Transistor.jpg',
+          creator: 'Korn',
+          descrizione: 'Hey you, hey you, devil\'s little sister\nListening to your twisted transistor\nHold it between your legs\nTurn it up, turn it up\nThe wind is coming through\nCan\'t get enough...',
+          id: '003',
+          tipo:'musica'
+    },
+    {
+        titolo: 'League of Legends',
+        immagine: 'https://www.outplayed.it/wp-content/uploads/2019/09/league-of-legends.jpg',
+        creator: 'Riot',
+        descrizione: 'League of Legends is a 2009 multiplayer online battle arena video game developed and published by Riot Games. Inspired by Defense of the Ancients, a custom map for Warcraft III...',
+        id: '001',
+        tipo: 'gameplay'
+    },
+    {
+        titolo: 'Minecraft',
+        immagine: 'https://d2e111jq13me73.cloudfront.net/sites/default/files/styles/amp_blog_image_large/public/blog/csm-blog/parentsultimateguide-minecraft-blog.jpg?itok=2beDaNo7',
+        creator: 'Mojang',
+        descrizione: 'Minecraft is a sandbox video game developed by Mojang. The game was created by Markus "Notch" Persson in the Java programming language. Following several early test versions...',
+        id: '002',
+        tipo: 'gameplay'
+    },
+    {
+        titolo: 'Cyberpunk 2077',
+        immagine: 'https://www.gamelegends.it/wp-content/uploads/2021/01/cyberpunk-2077-Main-Theme.jpg',
+        creator: 'CD Projekt RED',
+        descrizione: 'Cyberpunk 2077 is an open-world, action-adventure role-playing video game, developed and published by CD Projekt RED that was released December 10, 2020. The game was announced during the 2012 CD Projekt Red Summer Conference...',
+        id: '003',
+        tipo: 'gameplay'
+    }]
