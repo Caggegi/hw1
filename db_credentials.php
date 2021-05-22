64f6a67e02bc551917ee2fb2b4ee331d2ebc1196
Caggegi <rosariocaggegi99@gmail.com>
2021-05-22 20:34:15 +0200

    prova

--- db_credentials.php
+++ db_credentials.php
@@ -8,13 +8,3 @@
     );
     //22/05/2021
 ?>
-=======
-<?php
-    $mydb_connect=array(
-        'server' => 'localhost',
-        'user' => 'root',
-        'psw' => '',
-        'db' => 'vt'
-    );
-?>
->>>>>>> c8af1445bccefbdaf3e9bbaadef53fffe475d24f
